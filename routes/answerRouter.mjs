66df8b6f0861950deedc30c2fc6b3565c5ad33fd
Arkorn Pattanavibul <arkorn.pat@gmail.com>
2024-11-14 08:35:39 +0700

    feat:add validation for vote values

--- routes/answerRouter.mjs
+++ routes/answerRouter.mjs
@@ -5,24 +5,27 @@ const answerRouter = Router();
 
 //vote answer
 answerRouter.post("/:answerId/vote", async (req, res) => {
-    const answerId = req.params.answerId;
-    const newVote = req.body;
-    if(!newVote.vote){
-        return res.status(400).json({"message": "Invalid vote value."})
-      }
-    try {
-      const query = `insert into answer_votes (answer_id , vote) values($1, $2)`;
-      const values = [answerId, newVote.vote];
-      await connectionPool.query(query, values);
-      return res.status(201).json({
-        message: "Vote on the answer has been recorded successfully.",
-      });
-    } catch (e){
-        console.log(e)
-      return res.status(500).json({
-        message: "Server could not create votes because database connection",
-      });
-    }
-  });
+  const answerId = req.params.answerId;
+  const newVote = req.body;
+  if (!newVote.vote) {
+    return res.status(400).json({ message: "Invalid vote value." });
+  }
+  if (newVote.vote !== 1 && newVote.vote !== -1) {
+    return res.status(400).json({ message: "Invalid vote value." });
+  }
+  try {
+    const query = `insert into answer_votes (answer_id , vote) values($1, $2)`;
+    const values = [answerId, newVote.vote];
+    await connectionPool.query(query, values);
+    return res.status(201).json({
+      message: "Vote on the answer has been recorded successfully.",
+    });
+  } catch (e) {
+    console.log(e);
+    return res.status(500).json({
+      message: "Server could not create votes because database connection",
+    });
+  }
+});
 
-export default answerRouter
+export default answerRouter;
--- routes/questionRouter.mjs
+++ routes/questionRouter.mjs
@@ -104,13 +104,12 @@ questionRouter.put("/:questionId", [validateDataQuestion], async (req, res) => {
 
     const results = await connectionPool.query(query, values);
 
-    if(!results.rows[0]){
-        return res.status(404).json({"message": "Question not found."})
+    if (!results.rows[0]) {
+      return res.status(404).json({ message: "Question not found." });
     }
 
-
     return res.status(200).json({
-      message: "Updated question sucessfully"
+      message: "Updated question sucessfully",
     });
   } catch {
     return res.status(500).json({
@@ -126,8 +125,8 @@ questionRouter.delete("/:questionId", async (req, res) => {
     const query = `delete from questions where id = $1`;
     const values = [questionId];
     const results = await connectionPool.query(query, values);
-    if(!results.rows[0]){
-        return res.status(404).json({"message": "Question not found."})
+    if (!results.rows[0]) {
+      return res.status(404).json({ message: "Question not found." });
     }
     return res
       .status(200)
@@ -143,17 +142,23 @@ questionRouter.delete("/:questionId", async (req, res) => {
 questionRouter.post("/:questionId/answer", async (req, res) => {
   const questionId = req.params.questionId;
   const newAnswer = req.body;
-  if(!newAnswer.content){
-    return res.status(400).json({"message": "Invalid request data."})
+  if (!newAnswer.content) {
+    return res.status(400).json({ message: "Invalid request data." });
   }
-  if(newAnswer.content.length > 300){
-    return res.status(400).json({"message": "Answer content exceeds the maximum allowed length of 300 characters."})
+  if (newAnswer.content.length > 300) {
+    return res.status(400).json({
+      message:
+        "Answer content exceeds the maximum allowed length of 300 characters.",
+    });
   }
 
   try {
-    const results = await connectionPool.query(`select questions.id from questions where id =$1`, [questionId])
-    if(!results.rows[0]){
-        return res.status(404).json({"message": "Question not found."})
+    const results = await connectionPool.query(
+      `select questions.id from questions where id =$1`,
+      [questionId]
+    );
+    if (!results.rows[0]) {
+      return res.status(404).json({ message: "Question not found." });
     }
     await connectionPool.query(
       `insert into answers (question_id, content) values ($1,$2)`,
@@ -172,9 +177,12 @@ questionRouter.post("/:questionId/answer", async (req, res) => {
 questionRouter.get("/:questionId/answer", async (req, res) => {
   const questionId = req.params.questionId;
   try {
-    const result = await connectionPool.query(`select questions.id from questions where id =$1`, [questionId])
-    if(!result.rows[0]){
-        return res.status(404).json({"message": "Question not found."})
+    const result = await connectionPool.query(
+      `select questions.id from questions where id =$1`,
+      [questionId]
+    );
+    if (!result.rows[0]) {
+      return res.status(404).json({ message: "Question not found." });
     }
 
     const results = await connectionPool.query(
@@ -193,9 +201,12 @@ questionRouter.get("/:questionId/answer", async (req, res) => {
 questionRouter.delete("/:questionId/answer", async (req, res) => {
   const questionId = req.params.questionId;
   try {
-    const results = await connectionPool.query(`select questions.id from questions where id =$1`, [questionId])
-    if(!results.rows[0]){
-        return res.status(404).json({"message": "Question not found."})
+    const results = await connectionPool.query(
+      `select questions.id from questions where id =$1`,
+      [questionId]
+    );
+    if (!results.rows[0]) {
+      return res.status(404).json({ message: "Question not found." });
     }
     await connectionPool.query(`delete from answers where question_id = $1`, [
       questionId,
@@ -214,13 +225,20 @@ questionRouter.delete("/:questionId/answer", async (req, res) => {
 questionRouter.post("/:questionId/vote", async (req, res) => {
   const questionId = req.params.questionId;
   const newVote = req.body;
-  if(!newVote.vote){
-    return res.status(400).json({"message": "Invalid vote value."})
+  if (!newVote.vote) {
+    return res.status(400).json({ message: "Invalid vote value." });
   }
+  if (newVote.vote !== 1 && newVote.vote !== -1) {
+    return res.status(400).json({ message: "Invalid vote value." });
+  }
+
   try {
-    const results = await connectionPool.query(`select questions.id from questions where id =$1`, [questionId])
-    if(!results.rows[0]){
-        return res.status(404).json({"message": "Question not found."})
+    const results = await connectionPool.query(
+      `select questions.id from questions where id =$1`,
+      [questionId]
+    );
+    if (!results.rows[0]) {
+      return res.status(404).json({ message: "Question not found." });
     }
     const query = `insert into question_votes (question_id , vote) values($1, $2)`;
     const values = [questionId, newVote.vote];
